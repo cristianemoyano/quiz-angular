4af7a72d1f5d19bb550c1d410e386d58a8f9d237
cmoyano <cristianmoyano.mza@gmail.com>
2022-07-23 20:36:59 -0300

    first screen

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -6,17 +6,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
-    // AngularFireModule.initializeApp(environment)
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- mobile/src/app/tab1/tab1.page.ts
+++ mobile/src/app/tab1/tab1.page.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Game } from '../model/game.model';
+import { GameService } from '../service/game.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  welcomeForm: FormGroup;
+
+  @ViewChild('name') nameKey!: ElementRef;
+  @ViewChild('totalQuestions') totalQuestionsKey!: ElementRef;
+  @ViewChild('category') categoryKey!: ElementRef;
+  @ViewChild('level') levelKey!: ElementRef;
+  @ViewChild('questionType') questionTypeKey!: ElementRef;
+  constructor(private formBuilder: FormBuilder, private router: Router, private gameService: GameService) {
+    this.welcomeForm = this.formBuilder.group({
+      name: ['',[Validators.required, Validators.min(4)]],
+      totalQuestions: [10,[Validators.required]],
+      category: ['Conocimiento General',[Validators.required]],
+      level: ['Fácil',[Validators.required]],
+      questionType: ['Aleatoria',[Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreate(response ) {
+    localStorage.setItem("gameID", response.id);
+    console.log("Game ID: "+response.id);
+  }
+  onError(error) {
+    console.log(error);
+  }
+
+  startQuiz() {
+    this.gameService.createGame({user: this.welcomeForm.controls['name'].value} as Game, this.onCreate, this.onError);
+    localStorage.setItem("name", this.welcomeForm.controls['name'].value);
+
+    // localStorage.setItem("totalQuestions", this.totalQuestionsKey.nativeElement.value)
+    // localStorage.setItem("category", this.categoryKey.nativeElement.value)
+    // localStorage.setItem("level", this.levelKey.nativeElement.value)
+    // localStorage.setItem("questionType", this.questionTypeKey.nativeElement.value)
+
+    
+  }
+
+
+  onSubmit(): void {
+    this.welcomeForm.controls['name'].markAsTouched();
+    
+    if(!this.welcomeForm.invalid) {
+      this.startQuiz();
+      this.router.navigate(['/tabs/tab3']);
+    }
+  }
+
+
 
 }
